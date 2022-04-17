7cb28639119c5e9d3a2eb3fb62255aec86c3bfd5
Mate Farkas <mate.farkas@sch.hu>
2022-04-17 16:08:46 +0000

    Test image translation

--- qr.cpp
+++ qr.cpp
@@ -2,9 +2,8 @@
 #include "qrcodegen.hpp"
 
 static void draw_qr(UBYTE *image, const qrcodegen::QrCode &qr) {
-  int border = 4;
-  for (int j = -border; j < 2 * qr.getSize() + border; j++) {
-    for (int i = -border; i < 2 * qr.getSize() + border; i++) {
+  for (int j = 0; j < 2 * qr.getSize(); j++) {
+    for (int i = 0; i < 2 * qr.getSize(); i++) {
       if (qr.getModule(i / 2, j / 2))
         image[i * LINE_STRIDE + j / 8] &= ~(1UL << (7 - j % 8));
       else
--- tests/test_qr.cpp
+++ tests/test_qr.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <fstream>
 
-#define UBYTE uint8_t
-#define UWORD uint16_t
-
 #include "../qr.cpp"
 
-UBYTE *Image;
+UBYTE *QrImage;
+UBYTE *FinalImage;
 UWORD Imagesize = ((WIDTH % 8 == 0) ? (WIDTH / 8) : (WIDTH / 8 + 1)) * HEIGHT;
 
 void dump_image(UBYTE *image) {
   for (int j = 0; j < WIDTH; j++) {
     for (int i = 0; i < HEIGHT; i++) {
-      bool black = image[i * LINE_STRIDE + j / 8] & 1UL << (7 - j % 8);
-      if (black)
+      if (image[i * LINE_STRIDE + j / 8] & 1UL << (7 - j % 8))
         std::cout << "#";
       else
         std::cout << "_";
@@ -23,14 +20,34 @@ void dump_image(UBYTE *image) {
   std::cout << std::endl;
 }
 
+void copy_image(UBYTE *from, UBYTE *to, int x, int y) {
+  for (int j = 0; j + x < WIDTH; j++) {
+    for (int i = 0; i + y < HEIGHT; i++) {
+      if (from[i * LINE_STRIDE + j / 8] & 1UL << (7 - j % 8))
+        to[(i + y) * LINE_STRIDE + (j + x) / 8] |= 1UL << (7 - (j + x) % 8);
+      else
+        to[(i + y) * LINE_STRIDE + (j + x) / 8] &= ~(1UL << (7 - (j + x) % 8));
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
-  if ((Image = (UBYTE *)malloc(Imagesize)) == NULL)
-    throw std::runtime_error("Failed to allocate eink memory");
+  if ((QrImage = (UBYTE *)malloc(Imagesize)) == NULL)
+    throw std::runtime_error("Failed to allocate QR memory");
+
+  if ((FinalImage = (UBYTE *)malloc(Imagesize)) == NULL)
+    throw std::runtime_error("Failed to allocate FINAL memory");
 
   const qrcodegen::QrCode qr = qrcodegen::QrCode::encodeText("https://indeed.sch.bme.hu/web/latest.jpg", qrcodegen::QrCode::Ecc::HIGH);
-  draw_qr(Image, qr);
+  draw_qr(QrImage, qr);
 
-  dump_image(Image);
+  copy_image(QrImage, FinalImage, 80, 110);
+  dump_image(FinalImage);
+
+  free(QrImage);
+  QrImage = NULL;
+  free(FinalImage);
+  FinalImage = NULL;
 
   return 0;
 }
